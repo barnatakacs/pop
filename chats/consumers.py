6fe25f25723e5505ba00db5d0a7b9e76ccb506b2
barnatakacs <takacsbarna17@gmail.com>
2024-10-18 19:31:29 +0200

    working chat, css fix needed still

--- chats/consumers.py
+++ chats/consumers.py
@@ -1,5 +1,8 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
+from asgiref.sync import sync_to_async
+from .models import Message, User, Chat
+from datetime import datetime
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -22,19 +25,39 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        message_content = text_data_json['content']
+
+        user = self.scope['user']
+        chat = self.chat_id
+
+        await self.save_message(user, chat, message_content)
 
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'content': message_content,
+                'user': self.scope['user'].username,
+                'created_at': self.now(),
             }
         )
 
     async def chat_message(self, event):
-        message = event['message']
+        message = event['content']
+        user = event['user']
+        created_at = event['created_at']
 
         await self.send(text_data=json.dumps({
-            'message': message
+            'content': message,
+            'user': user,
+            'created_at': created_at,
         }))
+
+    def now(self):
+        return datetime.now().strftime("%B %d, %Y, %I:%M %p")
+
+    @sync_to_async
+    def save_message(self, user, chat, message):
+        chat = Chat.objects.get(id=chat)
+
+        Message.objects.create(chat=chat, user=user, content=message)
